aa7558a2df23f34c0d6b33fbdd3b6c8e7cc65e97
Jared Hester <jared.hester@gmail.com>
2015-08-19 01:20:13 -0400

    Initial commit

---
+++ src/FsUnity.Core/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsUnity.Core")>]
+[<assembly: AssemblyProductAttribute("FsUnity.Core")>]
+[<assembly: AssemblyDescriptionAttribute("Backport of FSharp.Core for compatibility with Unity3d Game Engine")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
